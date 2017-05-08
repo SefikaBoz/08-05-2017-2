22918f6fd82014cc56c53a8cd4f47b0313372463
SefikaBoz <sefikaboz12@gmail.com>
2017-05-08 17:54:45 +0300

    Create ThreadSafeSingleton.cs

---
+++ ThreadSafeSingleton.cs
@@ -0,0 +1,31 @@
+# 08-05-2017-2
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo7
+{
+    public class ThreadSafeSingleton
+    {
+        private static ThreadSafeSingleton tt = new ThreadSafeSingleton();
+
+        //private static ThreadSafeSingleton()
+        //{
+
+        //}
+        private ThreadSafeSingleton()
+        {
+
+        }
+    
+        public static ThreadSafeSingleton TT
+        {
+            get{
+                return tt;
+            }
+        }
+    }
+}
